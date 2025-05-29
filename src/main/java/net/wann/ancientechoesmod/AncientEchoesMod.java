c774ea72dc33eb84beb249fac00ddbd27d1d095b
Wann <ridhwanhamimigans08@gmail.com>
2025-05-29 22:15:41 +0700

    Initial commit

---
+++ src/main/java/net/wann/ancientechoesmod/AncientEchoesMod.java
@@ -0,0 +1,16 @@
+package net.wann.ancientechoesmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AncientEchoesMod implements ModInitializer {
+	public static final String MOD_ID = "ancientechoesmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/wann/ancientechoesmod/AncientEchoesModClient.java
@@ -0,0 +1,10 @@
+package net.wann.ancientechoesmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AncientEchoesModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
